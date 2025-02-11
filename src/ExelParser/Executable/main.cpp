9af7072271a615f64c04f64bc9c179fa478e2604
CodeUser566 <Nikitapaladin2003@gmail.com>
2025-02-11 18:04:43 +0300

    изменено:      src/ExelParser/Executable/CMakeLists.txt 	изменено:      src/ExelParser/Executable/main.cpp

--- src/ExelParser/Executable/main.cpp
+++ src/ExelParser/Executable/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+#include "/home/gorillabacteria/.conan2/p/b/fmtaf509ab6c68e2/p/include/fmt/printf.h"
 
 using namespace std;
 
 int main()
 {
     std::cout<<"Hello World!";
-
+    fmt::printf("Hello!");
     return  0;
 }
