b6d23e2976c0d4a96f8bd475d8dd8535f6ab170b
CodeUser566 <Nikitapaladin2003@gmail.com>
2025-03-10 11:29:08 +0300

    изменено:      src/ExelParser/Executable/main.cpp 	новый файл:    src/Test.csv 	изменено:      "\320\270\320\264\320\265\320\270 \320\264\320\273\321\217 \320\277\321\200\320\276\320\265\320\272\321\202\320\260.txt"

--- src/ExelParser/Executable/main.cpp
+++ src/ExelParser/Executable/main.cpp
@@ -1,11 +1,57 @@
+#include <clocale>
+#include <fstream>
 #include <iostream>
-#include <xlnt/xlnt.hpp>
+#include <list>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    std::cout<<"Hello World!";
+int main() {
+  setlocale(LC_ALL, "ru");
 
-    return  0;
+
+  //обернуть в функцию, возможно создать класс
+  string Path;
+  string Line;
+  string acc;
+  ifstream CSV_Read;
+  list<string> Headers;
+
+  CSV_Read.exceptions(ifstream::badbit|ifstream::failbit); 
+  cout << "Введите путь до файла:";
+  cin >> Path;
+  try
+  {
+    CSV_Read.open(Path);
+  } 
+  catch (const ifstream::failure & ex) 
+  {
+    cout << "Ошибка открытия файла, Проверьте правильность написания файла.Также убедитесь что формат файла .CSV" << endl;
+    cout << ex.what() << endl;
+    cout << ex.code() << endl;
+  }
+
+ getline(CSV_Read,Line);
+
+
+for (int i = 0; i <= Line.length(); i++) 
+{
+  char CurrentSymbol = Line[i];
+
+  if (CurrentSymbol == ',')
+  {
+    Headers.push_front(acc);
+    acc.clear();
+    continue;
+  }
+  acc += CurrentSymbol;
+}
+if (!acc.empty()) 
+{
+  Headers.push_front(acc);
+}
+  
+
+CSV_Read.close();
+  return 0;
 }
